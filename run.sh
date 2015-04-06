12de95701d203a8710cb2facb0049f1ca4c79e85
Brian O'Neill <bone@alumni.brown.edu>
2015-04-06 17:17:43 -0400

    Adding run script.

---
+++ run.sh
@@ -0,0 +1,1 @@
+spark-submit --class com.github.boneill42.JavaDemo --master spark://127.0.0.1:7077 target/spark-on-cassandra-0.0.1-SNAPSHOT-jar-with-dependencies.jar spark://127.0.0.1:7077 127.0.0.1
